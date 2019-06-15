4de8366ccbb0f925381ff7a8a1376f3b6ee84214
Josh Tait <josh.tait3@gmail.com>
2019-06-15 16:50:20 +0100

    spit report into DOM

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
   declare(strict_types=1);
-
   require_once dirname(__DIR__) . '/vendor/autoload.php';
-  $helloWorld = new \CyberMohawk\HelloWorld();
-  $helloWorld->announce();
-  ?>
+  $damageReporter = new \CyberMohawk\DamageReporter();
+
+?>
+<!doctype html>
+  <h1> Hey! </h1>
+
+  <?php  $damageReporter->initalize(); ?>
+</html>
